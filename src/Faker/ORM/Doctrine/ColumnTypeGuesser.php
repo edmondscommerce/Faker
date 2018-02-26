a71194339f5ac24a39ca9d725d5307b5a3f2f312
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-02-26 07:29:11 +0000

    preventing issues with high precision floats causing randomNumber() can only generate numbers up to mt_getrandmax

--- src/Faker/ORM/Doctrine/ColumnTypeGuesser.php
+++ src/Faker/ORM/Doctrine/ColumnTypeGuesser.php
@@ -18,22 +18,31 @@ class ColumnTypeGuesser
 
     /**
      * @param ClassMetadata $class
+     *
      * @return \Closure|null
      */
     public function guessFormat($fieldName, ClassMetadata $class)
     {
         $generator = $this->generator;
-        $type = $class->getTypeOfField($fieldName);
+        $type      = $class->getTypeOfField($fieldName);
         switch ($type) {
             case 'boolean':
                 return function () use ($generator) {
                     return $generator->boolean;
                 };
             case 'decimal':
-                $size = isset($class->fieldMappings[$fieldName]['precision']) ? $class->fieldMappings[$fieldName]['precision'] : 2;
+                $nbDigits = isset($class->fieldMappings[$fieldName]['precision']) ? $class->fieldMappings[$fieldName]['precision'] : 2;
+                while ($nbDigits > 2) {
+                    $max = pow(10, $nbDigits);
+                    if ($max > mt_getrandmax()) {
+                        $nbDigits--;
+                        continue;
+                    }
+                    break;
+                }
 
-                return function () use ($generator, $size) {
-                    return $generator->randomNumber($size + 2) / 100;
+                return function () use ($generator, $nbDigits) {
+                    return $generator->randomNumber($nbDigits + 2) / 100;
                 };
             case 'smallint':
                 return function () {
@@ -49,7 +58,7 @@ class ColumnTypeGuesser
                 };
             case 'float':
                 return function () {
-                    return mt_rand(0, intval('4294967295'))/mt_rand(1, intval('4294967295'));
+                    return mt_rand(0, intval('4294967295')) / mt_rand(1, intval('4294967295'));
                 };
             case 'string':
                 $size = isset($class->fieldMappings[$fieldName]['length']) ? $class->fieldMappings[$fieldName]['length'] : 255;
